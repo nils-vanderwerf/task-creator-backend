b28632f14cc2e0047c558d3f28d61f0efb23b189
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-10 12:50:18 +1000

    Reseeded database

--- db/migrate/20210809095331_change_username_to_email.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUsernameToEmail < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :username, :email
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+    first_name: "Hans",
+    last_name: "Solo",
+    password: "asdf",
+    email: "hans@gmail.com"
+)
+
+User.create(
+    first_name: "Peter",
+    last_name: "Parker",
+    password: "asdf",
+    email: "asdf"
+)
+
+User.create(
+    first_name: "Connor Z",
+    last_name: "Rodgers",
+    password: "asdf",
+    email: "connorz@gmail.com"
+)
+
+User.create(
+    first_name: "Sam K",
+    last_name: " Mackerras",
+    password: "asdf",
+    email: "samk@gmail.com"
+)
+
+Task.create(
+  user_id: 3,
+  title: "Moving boxes",
+  description: 'I need help with moving 10 boxes to my apartment. You should be able to handle 150 lbs.',
+)
+
+Task.create(
+  user_id: 1,
+  title: "Mowing Lawn",
+  description: 'Getting pretty long',
+)
