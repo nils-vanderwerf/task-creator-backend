8149b1a41df617171652313d75f812fc7f2cae1b
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-20 16:05:38 +1000

    Test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,16 @@
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://localhost:3000",
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins "http://localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   allow do
-    origins "https://nils-task-creator-app.herokuapp.com/", "http://nils-task-creator-app.herokuapp.com/"
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins "https://nils-task-creator-app.herokuapp.com/"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
+
+
+
+
+
