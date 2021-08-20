92596cb03e36e244cf4c900ce0e19a6cf33e042c
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-20 15:44:33 +1000

    Tried to change urls for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "http://localhost:3000"
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins "https://nils-task-creator-app.herokuapp.com/"
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
