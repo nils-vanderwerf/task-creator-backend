0c1c231ccd9310e7dab6ead36d6196ddf9910996
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-20 23:23:06 +1000

    CChanged cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-<<<<<<< HEAD
     origins ["https://nils-task-creator-app.herokuapp.com/", "http://localhost:3000"]
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-=======
-    origins ["http://localhost:3000", "https://nils-task-creator-app.herokuapp.com/"]
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
->>>>>>> deploy
   end
 end
 
